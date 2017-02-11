608cb163681caf6e8ac46f28998e13bcbb5a2a6b
JeremyTurner1976 <jeremyturner1976@eagles.ewu.edu>
2017-02-11 01:58:02 -0700

    Solution Structure - Dapper, Most Recent .Net Core Libraries, and working MSTest in Core

---
+++ Angular2CoreBase.Common/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular2CoreBase.Common
+{
+	public class Class1
+	{
+		public Class1()
+		{
+		}
+
+		public string Ping() =>
+			"Pong";
+	}
+}
---
+++ Angular2CoreBase.Test/Common/Services/EmailServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Angular2CoreBase.Common;
+
+namespace Angular2CoreBase.Test.Common.Services
+{
+	[TestClass]
+	public class EmailServiceTests
+	{
+		[TestMethod]
+		public void GetSampleTest()
+		{
+			Assert.AreEqual("Pong", new Class1().Ping(), "Values do not match");
+		}
+	}
+}
